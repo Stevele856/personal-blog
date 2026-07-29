5adea2ab3119851528b7a7e625fc5c2e4c2dd87e
Steve <letrongvu.work@gmail.com>
2026-07-29 17:37:06 +0700

    test(main): wire view.Init/Render for manual verification

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/letrongvu/blog/internal/view"
+	"github.com/letrongvu/blog/web"
+)
+
+func main() {
+	if err := view.Init(web.Templates); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := view.Render(os.Stdout, "home.html", "world"); err != nil {
+		log.Fatal(err)
+	}
+}
